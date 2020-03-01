74db5f6f16ec803dc1a6ab0252897ac8f9db9358
Anton Nazin <anton.na987@gmail.com>
2020-03-01 21:20:30 +0300

    Oooh getopt

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "options.h"
 #include "server.h"
 #include <cassert>
+#include <getopt.h>
 #include <iostream>
 #include <signal.h>
 #include <unistd.h>
@@ -10,6 +11,26 @@ int main(int argc, char* argv[])
     signal(SIGHUP, SIG_IGN);
     auto daemon_res = daemon(0, 0);
     assert(daemon_res == 0);
+
+    // getopt dummy
+    int c;
+    int digit_optind = 0;
+
+    while (1) {
+        int this_option_optind = optind ? optind : 1;
+        int option_index = 0;
+        static struct option long_options[] = {
+            { "add", 1, 0, 0 },
+            { "append", 0, 0, 0 },
+            { "delete", 1, 0, 0 },
+            { "verbose", 0, 0, 0 },
+            { "create", 1, 0, 'c' },
+            { "file", 1, 0, 0 },
+            { 0, 0, 0, 0 }
+        };
+        c = getopt_long(argc, argv, "abc:d:012", long_options, &option_index);
+        break;
+    }
 
     Options options(argc, argv);
     if (options.help_requested()) {
